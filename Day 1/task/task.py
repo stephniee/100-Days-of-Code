29815de6b08811520d1877c75bb15b9d5cb93d6e
2212072 <2212072@slu.edu.ph>
2025-03-11 15:41:24 +0800

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Hello! Welcome to the Band Name Generator Project")
+city = input("May I ask where you are from?")
+petName = input("What's the name of your beloved pet?")
+print("Your band name could be: " + city + " " +petName)
