2820d1ef536ca61dfc953944ab9d0ebcb7484bf8
2212072 <2212072@slu.edu.ph>
2025-03-12 12:58:12 +0800

    Day 2

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,7 @@
-len(12345)
+len("12345")
+print(type("yuh"))
+print(type(21))
+print(type(3.14157))
+print(type(True))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
