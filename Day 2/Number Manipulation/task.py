dce8d04f33394a9774c83c7f8d68a766b46d4937
2212072 <2212072@slu.edu.ph>
2025-03-12 13:34:30 +0800

    Day 2

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,7 @@
-bmi = 84 / 1.65 ** 2
+# bmi = (84 / 1.65 ** 2)
+# bmi = round(bmi)
+#
+# print(f"Your bmi is: {bmi} ")
+score = 0
+score += 1
+print(score)
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -2,5 +2,7 @@ print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
+compute_tip = float((bill / people) * (float(tip / 100 + 1)))
 
-
+total_tip = round(compute_tip,2)
+print(f" Each person should pay: ${total_tip}")
