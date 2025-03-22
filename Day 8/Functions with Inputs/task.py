1bcf25969910e2735a44cdb1cfbbb0694c6a13c9
2212072 <2212072@slu.edu.ph>
2025-03-23 00:42:10 +0800

    Day 8 Ceaser Cipher

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,18 @@
+def greet():
+    print(f"Hello,")
+    print(f"How do you do? ")
+    print(f"Isn't the weather nice?")
+
+
+# First Method
+# friend = "Alice"
+# greet()
+
+# Second Method
+
+def greet_w_name(name):
+    print(f"Hello, {name}")
+    print(f"How do you do? {name}")
+    print(f"Isn't the weather nice? {name}")
+
+greet_w_name("Stephanie")
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,18 @@
 # Functions with input
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+# Function w/ 1 parameter
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
+
+# Function w/ 2 parameter
+def greet_with(name,location):
+    print(f"Hello! {name}, how is the weather in {location}?")
+    print(f"Oh, {name}, you live in {location} ")
 
 
-greet_with_name("Jack Bauer")
+greet_with(location="Ariana", name="London")
+
