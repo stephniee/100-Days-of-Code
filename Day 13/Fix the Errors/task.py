9fdecc59f07ccb0153bd2ee815bd91c2b33288f7
2212072 <2212072@slu.edu.ph>
2025-03-27 13:33:52 +0800

    Day 13 - Debugging

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,8 @@
-age = int(input("How old are you?"))
-if age > 18:
-print("You can drive at age {age}.")
+try:
+    age = int(input("How old are you?"))
+    if age > 18:
+        print(f"You can drive at age {age}.")
+    else:
+        print("You have to wait till you're above 18 to drive!")
+except TypeError:
+    print("You cant mix integers and strings in a comparison")
---
+++ Day 13/Use a Debugger/Solutions for Exercises.py
@@ -0,0 +1,29 @@
+# Debugging odd or even
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+# Checks for Leap Year
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        return True
+    else:
+        return False
+# Checks for Fizz Buzz
+# Target is the number up to which we count
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
